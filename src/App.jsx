28e90739aa19960c413660119ef0d57942d67d98
Virginia Mitchell <ginny.e.mitchell@gmail.com>
2023-04-28 13:38:49 -0500

    added in dice components and started layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import React from "react";
+import Die from "./Components/Die";
+import { nanoid } from "nanoid";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [dice, setDice] = React.useState(allNewDice());
+
+  //generates first roll of each round
+  function allNewDice() {
+    const diceArray = new Array(6).fill().map(() => {
+      return {
+        value: Math.floor(Math.random() * 6 + 1),
+        isHeld: false,
+        id: nanoid(),
+      };
+    });
+    return diceArray;
+  }
+
+  function holdDice(id) {
+    setDice((prevDice) =>
+      prevDice.map((die) => {
+        return id === die.id ? { ...die, isHeld: !die.isHeld } : { ...die };
+      })
+    );
+  }
+
+  const diceElements = dice.map((die) => (
+    <Die
+      key={die.id}
+      id={die.id}
+      value={die.value}
+      isHeld={die.isHeld}
+      holdDice={() => holdDice(die.id)}
+    />
+  ));
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <main>
+        <div className="dice-container">
+          <div className="instructions">Here will be the instructions</div>
+          <div className="roller-holder">{diceElements}</div>
+          <button>Roll Em'</button>
+        </div>
+        <h1>I will hold scoresheet component</h1>
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
